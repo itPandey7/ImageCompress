c57c211c49887dfa9f9b9e620e0fa5525d3cfe27
Ami404 <itpandey7@gmail.com>
2020-01-25 17:00:07 +0530

    begin

---
+++ app/src/main/java/com/raywenderlich/android/razegalactic/Constraiints.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.android.razegalactic
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Constraiints : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.practice)
+    }
+}
